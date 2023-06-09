e27bebc63ea963a295c194b68846e1f235655ae1
Ferdinand Majelan <ferdinand.mazhelan@gmail.com>
2023-06-09 11:58:22 -0400

    sub_systems: start adding camera streams

--- src/Camera.cpp
+++ src/Camera.cpp
@@ -4,38 +4,77 @@
 #include <chrono>
 #include "Camera.h"
 
+/*BEGIN_EVENT_TABLE(Camera, wxEvtHandler)
+        EVT_TIMER(wxID_ANY, Camera::OnTimer)
+END_EVENT_TABLE()*/
+
 wxPanel* Camera::mainPanel = nullptr;
 
-void Camera::initMainPanel(wxWindow* parent) {
+Camera::Camera(int id): id_(id), cap(0)
+{
+    cap.set(cv::CAP_PROP_FRAME_WIDTH, 640);
+    cap.set(cv::CAP_PROP_FRAME_HEIGHT, 480);
+
+    this->Connect(wxID_ANY, wxEVT_TIMER, wxTimerEventHandler(Camera::OnTimer));
+    timer_.SetOwner(this);
+    timer_.Start(TIMERVALUE, true);
+}
+
+void Camera::initMainPanel(wxWindow* parent)
+{
     mainPanel = new wxPanel(parent, wxID_ANY);
 }
 
-wxPanel* Camera::getMainPanel() {
+wxPanel* Camera::getMainPanel()
+{
     return mainPanel;
 }
 
 void Camera::simulate()
 {
-	//camera objects load their streams in parallel, for now just load an image
-	threads.emplace_back(std::thread(&Camera::load, this));
+    //camera objects load their streams in parallel, for now just load an image
+    threads.emplace_back(std::thread(&Camera::load, this));
 }
 
 void Camera::load()
 {
-	// sleep at every iteration to reduce CPU usage
+    // sleep at every iteration to reduce CPU usage
     std::this_thread::sleep_for(std::chrono::milliseconds(1));
-    std::string image_name = std::to_string(getID()+1) + ".png";
-	image_ = wxImage("1.png", wxBITMAP_TYPE_PNG);
-	if ( !image_.IsOk() )
+
+    std::string image_name = std::to_string(getID()) + ".png";
+    //wxMessageOutputDebug().Printf("getID %d", getID());
+    std::cout << image_name << std::endl;
+    //image_ = wxImage(image_name, wxBITMAP_TYPE_PNG);
+    if ( !image_.IsOk() )
     {
-    	// need to lock here, accessed by multiple threads
-    	std::cout << "Can't load image " << image_name << std::endl;
+        // need to lock here, accessed by multiple threads
+        std::cout << "Can't load image " << image_name << std::endl;
     }
-	//panel_ = new wxPanel(mainPanel, wxID_ANY);
+    //panel_ = new wxPanel(mainPanel, wxID_ANY);
 
 }
 
 wxImage Camera::getImage()
 {
-	return image_;
+    return image_;
+}
+
+
+void Camera::OnTimer(wxTimerEvent& event)
+{
+    if (cap.isOpened())
+    {
+        cv::Mat frame;
+
+        cap >> frame; // get a new frame from camera
+        cv::cvtColor(frame, frame, cv::COLOR_BGR2RGB);
+        image_ = wxImage(frame.cols, frame.rows, frame.data, true);
+
+    }
+
+    else
+    {
+        std::cout << "Can't load camera " << std::endl;
+    }
+
 }
--- src/Camera.h
+++ src/Camera.h
@@ -2,12 +2,14 @@
 #define CAMERA_H
 
 #include <wx/wx.h>
+#include <wx/timer.h>
+#include <opencv2/opencv.hpp>
 #include "MonitorObject.h"
 
-class Camera: public MonitorObject
+class Camera: public MonitorObject, public wxEvtHandler
 {
 public:
-	Camera(int id) {id_ = id;};
+	Camera(int id);
 	void simulate(); //
 	int getID() {return id_;};
 	static void initMainPanel(wxWindow* parent);
@@ -17,10 +19,20 @@ public:
 private:
 	static wxPanel *mainPanel;
 	void load(); // load image from file
+	void OnTimer(wxTimerEvent& event);
 	int id_;
 	wxImage image_;
 	wxPanel *panel_;
 	wxBitmap scaledBg_;
+
+	cv::VideoCapture cap;
+	wxTimer timer_;
+	wxBitmap *bitmap_;
+
+	static constexpr unsigned int FPS{25};
+	static constexpr unsigned int TIMERVALUE{1000/FPS};
+
+	//DECLARE_EVENT_TABLE();
 };
 
 #endif
